89c0645b80186b928357e11f0410470817f70ead
AndreSanchezSilva <andre@worldimportados.com.br>
2013-10-14 22:55:50 -0300

    Conexao

--- DAO/Conexao.php
+++ DAO/Conexao.php
@@ -10,7 +10,8 @@ class Conexao extends PDO {
     public static function conecta() {
         if(!isset( self::$conexao )){
             try {
-                self::$conexao = new Conexao("mysql:host=localhost;dbname=ecommerce", "root", "");
+                self::$conexao = new Conexao("pgsql:host=ec2-23-21-133-106.compute-1.amazonaws.com
+;dbname=dfjamjie4vlfeq", "kxyxzoxjfjjbzl", "_I7fyDJgFOoKSRoaBbSsJpMFur");
             } catch ( Exception $e ) {
                 echo 'Erro ao conectar';
                 exit ();
