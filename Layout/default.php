f36d61fa780c1e549c07108bdf275c097f46a3f4
AndreSanchezSilva <andre@worldimportados.com.br>
2013-11-24 16:11:08 -0200

    ..

--- Layout/default.php
+++ Layout/default.php
@@ -13,33 +13,6 @@
 </head>
 <body>
 <header class="container">
-<<<<<<< HEAD
-	<div class="row">
-		<div class="span12">
-			<h1>Aplicação</h1>
-		</div>
-	</div>
-	<div class="row">
-		<div class="span12">
-			<nav>
-				<ul>
-					<li>
-						<a href="Grupo/onLista">Grupo</a>
-						<ul>
-							<li><a href="Grupo/onCadastro">Cadastro</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="Subgrupo/onView">Subgrupo</a>
-					</li>
-					<li>
-						<a href="Produto/onView">Produto</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-=======
 	<nav class="navbar navbar-default" role="navigation">
 	  <!-- Brand and toggle get grouped for better mobile display -->
 	  <div class="navbar-header">
@@ -76,8 +49,6 @@
 	    </ul>
 	  </div><!-- /.navbar-collapse -->
 	</nav>
-
->>>>>>> f0fe6d2cc7524016a458615175aa6ab3a3157403
 </header>
 <main class="container">
 {{content}}
